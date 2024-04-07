82a9da4d21e3333356892e33bb1ed43d2cc750bb
Maisa Preis <maisapierinipreis@gmail.com>
2024-04-07 20:39:15 -0300

    style(front): Improvements on Calendar layout.

--- frontend/src/app/pages/calendar/appointments.jsx
+++ frontend/src/app/pages/calendar/appointments.jsx
@@ -1,14 +1,22 @@
 import styles from "./Calendar.module.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCircleInfo } from '@fortawesome/free-solid-svg-icons';
 
 export default function Appointments({ time, appointments }) {
-    return (
-      <>
-        <div className={`${styles.schedule} ${styles.blue}`}>{time}</div>
-        {appointments.map((appointment, index) => (
-          <button key={index} className={`${styles.schedule} ${styles.graylight}`}>
-            {appointment}
-          </button>
-        ))}
-      </>
-    );
-  }
+  return (
+    <>
+      <div className={`${styles.schedule} ${styles.blue}`}>{time}</div>
+      {appointments.map((appointment, index) => (
+        <button key={index} className={`${styles.schedule} ${styles.graylight}`}>
+          <div className="flex justify-between">
+            <span className="flex justify-center w-3/4">{appointment}</span>
+            {appointment ?
+              <FontAwesomeIcon icon={faCircleInfo} className={styles.info} />
+              : <span></span>
+            }
+          </div>
+        </button>
+      ))}
+    </>
+  );
+}
--- frontend/src/app/pages/calendar/page.tsx
+++ frontend/src/app/pages/calendar/page.tsx
@@ -10,26 +10,30 @@ const daysOfWeek = [
   { dayWeek: "Sáb", day: "23/03" },
 ];
 
+// TODO: Receber o nome completo do backend, e criar funcão para
+// adiconar nome abreviado.
+
 const appointments = [
-  { time: "09:00", appointments: ["", "", "Luis Silva", "", "Amanda Lopes", ""] },
-  { time: "10:00", appointments: ["", "Renan", "", "", "", ""] },
-  { time: "11:00", appointments: ["", "", "Luis Silva", "", "Amanda Lopes", ""] },
+  { time: "09:00", appointments: ["", "", "Luis Silva", "", "Amanda L.", ""] },
+  { time: "10:00", appointments: ["", "Renan B.", "", "", "", ""] },
+  { time: "11:00", appointments: ["", "", "Luis Silva", "", "Amanda L.", ""] },
   { time: "13:00", appointments: ["Bianca", "", "Paulo", "", "", ""] },
   { time: "14:00", appointments: ["", "", "", "", "", ""] },
-  { time: "15:00", appointments: ["", "", "Luis Silva", "", "Amanda Lopes", ""] },
+  { time: "15:00", appointments: ["", "", "Luis Silva", "", "Amanda L.", ""] },
   { time: "16:00", appointments: ["", "José", "", "", "Lopes", ""] },
-  { time: "17:00", appointments: ["", "", "Luis Silva", "", "André Lopes", ""] },
+  { time: "17:00", appointments: ["", "", "Maisa P. Preis", "", "André Lopes", ""] },
   { time: "18:00", appointments: ["", "", "", "", "Bia", ""] },
 ]
 
 export default function Calendar() {
   return (
     <div className="content">
-      <div className="grid grid-cols-7 gap-3">
-        <div className={`${styles.week} ${styles.lightblue}`}>Horários</div>
+      <div className={styles.grid}>
+        <div className={`${styles.week} ${styles.time}`}>Horários</div>
         {daysOfWeek.map((day, index) => (
-          <div key={index} className={`${styles.week} ${styles.bluedark}`}>
-            {day.dayWeek} - {day.day}
+          <div key={index} className={`${styles.week} ${styles.header}`}>
+            <span>{day.dayWeek}</span>
+            <span>{day.day}</span>
           </div>
         ))}
         {appointments.map((appointment) => (
